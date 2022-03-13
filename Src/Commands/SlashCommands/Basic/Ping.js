f6187df350916351257bb497dc273fc652e8bf03
RileCraft <rilecraft@gmail.com>
2022-03-14 00:23:12 +0530

    Ping Command

--- Src/Commands/SlashCommands/Basic/Ping.js
+++ Src/Commands/SlashCommands/Basic/Ping.js
@@ -1,13 +1,19 @@
+const pingSchema = require("../../../Models/Cooldown")
 module.exports = {
 	name: "ping",
 	guilds: ["861138548328366100"],
 	description: "Run this to see my ping.",
 	run: async(client, interaction, container) => {
-		const ping = new container.Discord.MessageEmbed()
+		const oldDate = Date.now()
+		await pingSchema.findOne({ userID: interaction.user.id })
+		const newDate = Date.now()
+        const ping = new container.Discord.MessageEmbed()
 		.setColor('RANDOM')
 		.setTimestamp()
-		.setTitle('🏓╎ Pong!')
-		.setDescription(`🏠╎Websocket Latency: ${client.ws.ping}ms\n🤖╎Bot Latency: ${Date.now() - interaction.createdTimestamp}ms`);
-		interaction.reply({ embeds: [ping] })
+        .setTitle('🏓╎ Pong!')
+		.setDescription(`🏠╎Websocket Latency: ${client.ws.ping}ms\n🤖╎Bot Latency: ${Date.now() - interaction.createdTimestamp}ms\n🎲╎Database Ping: ${Math.floor(newDate - oldDate)}ms`);
+		interaction.reply({
+			embeds: [ping]
+		})
 	}
 }
